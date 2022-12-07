092bc422663bf28e194fa9ad4a8659ff06599c65
Bolakale <bolakalebb@gmail.com>
2022-12-07 12:20:05 -0800

    elements

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 3-common_elements.py
+# Bolakale Aduloju
+
+
+def common_elements(set_1, set_2):
+    """Return a set of common elements in two sets."""
+    return (set_1 & set_2)
