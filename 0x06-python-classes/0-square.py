ef20ffc475df5a84034aaae928eea1fdffbb8c1e
Bolakale <bolakalebb@gmail.com>
2022-12-21 08:00:36 -0800

    task 0-6

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+@author: Bolakale Aduloju
+"""
+
+
+class Square:
+    """Empty class Square that defines a square
+
+    Attributes:
+        empty
+    """
+    pass
