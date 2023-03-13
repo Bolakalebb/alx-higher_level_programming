fae2d210bc69e5c5105e83b021d66a3e0ab58e68
Bolakale <bolakalebb@gmail.com>
2023-03-13 14:30:34 -0700

    biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,2 +1,9 @@
 #!/usr/bin/node
-myVar = 333;
+const myVar = process.argv.length;
+if (myVar <= 3) {
+  console.log(0);
+} else {
+  const args = process.argv.slice(2).map(x => parseInt(x));
+  const sorted = args.sort((a, b) => a < b);
+  console.log(sorted[1]);
+}
